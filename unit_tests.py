e022626f7c607d225ba5a406ac17714db099a69b
Michael Penkov <misha.penkov@gmail.com>
2014-07-22 16:08:36 +0900

    added more YouTube ID unit tests

--- unit_tests.py
+++ unit_tests.py
@@ -10,6 +10,26 @@ class TestExtractYouTubeId(unittest.TestCase):
         url = "https://www.youtube.com/watch?v=V5E8kDo2n6g&amp;feature=youtu.be"
         self.assertEquals(extract_youtube_id(url), "V5E8kDo2n6g")
 
+    def test_long(self):
+        url = "http://www.youtube.com/watch?feature=player_embedded&amp;v=LEN5rn47gYQ"
+        self.assertEquals(extract_youtube_id(url), "LEN5rn47gYQ")
+
+    def test_underscore(self):
+        url = "https://www.youtube.com/watch?v=-8_0eAME3Xw"
+        self.assertEquals(extract_youtube_id(url), "8_0eAME3Xw")
+
+    def test_hyphen(self):
+        url = "http://www.youtube.com/watch?v=N-gPAMeXlQk"
+        self.assertEquals(extract_youtube_id(url), "N-gPAMeXlQk")
+
+    def test_short(self):
+        url = "http://youtu.be/co9IZOSssFw"
+        self.assertEquals(extract_youtube_id(url), "co9IZOSssFw")
+
+    def test_short2(self):
+        url = "http://youtu.be/Cy0RPWK_5wg"
+        self.assertEquals(extract_youtube_id(url), "Cy0RPWK_5wg")
+
     def test_negative(self):
         url = "http://i.imgur.com/KJ0h3nZ.png"
         self.assertEquals(extract_youtube_id(url), None)
