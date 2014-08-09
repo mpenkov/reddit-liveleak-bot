27cba1740812ea73424683e22777d3ea385e99d6
Michael Penkov <misha.penkov@gmail.com>
2014-08-10 00:54:34 +0900

    adding logging to video_exists.py

--- video_exists.py
+++ video_exists.py
@@ -1,13 +1,20 @@
 """Check if the video with a specific YouTube ID still exists."""
 import requests
 import json
+import logging
 from user_agent import USER_AGENT
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__file__)
+logger.setLevel(logging.DEBUG)
+
 def video_exists(developer_key, youtube_id):
+    meth_name = "video_exists"
     url = "https://www.googleapis.com/youtube/v3/videos"
     headers = {"User-Agent": USER_AGENT}
     params = {"key": developer_key, "part": "id", "id": youtube_id}
-    r = requests.get(url, params=params)
+    r = requests.get(url, params=params, headers=headers)
+    logger.debug("%s: status_code: %d", meth_name, r.status_code)
     assert r.status_code == 200
     obj = json.loads(r.text)
     return obj["pageInfo"]["totalResults"] > 0
@@ -17,4 +24,4 @@ if __name__ == "__main__":
     import yaml
     with open("config.yml") as fin:
         doc = yaml.load(fin)
-    print exists(doc["google_developer_key"], sys.argv[1])
+    print video_exists(doc["google_developer_key"], sys.argv[1])
