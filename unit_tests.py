6365373a8a89e0ed819cc70ea01a20ccc361a6d3
Michael Penkov <misha.penkov@gmail.com>
2014-08-21 16:44:39 +0900

    fixing code formatting

--- unit_tests.py
+++ unit_tests.py
@@ -2,15 +2,22 @@ import unittest
 import os.path as P
 import yaml
 
+from liveleak_upload import LiveLeakUploader
 from bot import extract_youtube_id, MENTION_REGEX
 from liveleak_upload import extract_multipart_params
+from video_exists import video_exists
+
+CURRENT_DIR = P.dirname(P.abspath(__file__))
+
 
 class TestMentionRegex(unittest.TestCase):
+
     def test_positive(self):
         text = "redditliveleakbot +repost"
         self.assertIsNotNone(MENTION_REGEX.search(text))
 
     def test_negative(self):
+
         text = "/u/redditliveleakbot"
         self.assertIsNone(MENTION_REGEX.search(text))
 
@@ -20,17 +27,21 @@ class TestMentionRegex(unittest.TestCase):
         text = "redditliveleakbot repost"
         self.assertIsNone(MENTION_REGEX.search(text))
 
+
 class TestExtractYouTubeId(unittest.TestCase):
+
     def test_positive(self):
         url = "https://www.youtube.com/watch?v=IU5NSSzYygk"
         self.assertEquals(extract_youtube_id(url), "IU5NSSzYygk")
 
     def test_querystring(self):
-        url = "https://www.youtube.com/watch?v=V5E8kDo2n6g&amp;feature=youtu.be"
+        url = "https://www.youtube.com/watch?v=V5E8kDo2n6g&amp;\
+feature=youtu.be"
         self.assertEquals(extract_youtube_id(url), "V5E8kDo2n6g")
 
     def test_long(self):
-        url = "http://www.youtube.com/watch?feature=player_embedded&amp;v=LEN5rn47gYQ"
+        url = "http://www.youtube.com/watch?feature=player_embedded&amp;\
+v=LEN5rn47gYQ"
         self.assertEquals(extract_youtube_id(url), "LEN5rn47gYQ")
 
     def test_underscore(self):
@@ -56,10 +67,10 @@ class TestExtractYouTubeId(unittest.TestCase):
         url = "https://twitter.com/Praporec/status/489524665723809792/photo/1"
         self.assertEquals(extract_youtube_id(url), None)
 
-from video_exists import video_exists
+
 class TestVideoExists(unittest.TestCase):
     def setUp(self):
-        with open(P.join(P.dirname(P.abspath(__file__)), "config.yml")) as fin:
+        with open(P.join(CURRENT_DIR, "config.yml")) as fin:
             self.key = yaml.load(fin)["google_developer_key"]
 
     def test_positive(self):
@@ -70,17 +81,17 @@ class TestVideoExists(unittest.TestCase):
         video_id = "Y7UmFIpenjs"
         self.assertFalse(video_exists(self.key, video_id))
 
+
 class TestUpload(unittest.TestCase):
     def setUp(self):
-        from liveleak_upload import LiveLeakUploader
         self.up = LiveLeakUploader(True)
 
-        with open(P.join(P.dirname(P.abspath(__file__)), "config.yml")) as fin:
+        with open(P.join(CURRENT_DIR, "config.yml")) as fin:
             doc = yaml.load(fin)
         self.up.login(doc["liveleak"]["username"], doc["liveleak"]["password"])
 
     def test_upload(self):
-        path = P.join(P.dirname(P.abspath(__file__)), "test", "foreman_cif.mp4")
+        path = P.join(CURRENT_DIR, "test", "foreman_cif.mp4")
         self.assertTrue(P.isfile(path))
 
         self.up.upload(path, "test", "test", "test", "Other")
@@ -91,8 +102,9 @@ class TestUpload(unittest.TestCase):
 
 
 class TestMultipartParams(unittest.TestCase):
+
     def test_parse(self):
-        path = P.join(P.dirname(P.abspath(__file__)), "test", "add_item.html")
+        path = P.join(CURRENT_DIR, "test", "add_item.html")
         with open(path) as fin:
             html = fin.read()
         p = extract_multipart_params(html)
@@ -109,5 +121,11 @@ class TestMultipartParams(unittest.TestCase):
         self.assertEqual(p["AWSAccessKeyId"], "AKIAIWBZFTE3KNSLSTTQ")
         self.assertEqual(
             p["policy"],
-            "eyJleHBpcmF0aW9uIjoiMjAxNC0wNy0xN1QyMjozMjoxOC4wMDBaIiwiY29uZGl0aW9ucyI6W3siYnVja2V0IjoibGxidWNzIn0seyJhY2wiOiJwcml2YXRlIn0seyJFeHBpcmVzIjoiVGh1LCAwMSBKYW4gMjAzNyAxNjowMDowMCBHTVQifSxbInN0YXJ0cy13aXRoIiwiJGtleSIsIjIwMTRcL0p1bFwvMTZcL0xpdmVMZWFrLWRvdC1jb20tMmYzXzE0MDU1NjQzMzgiXSxbInN0YXJ0cy13aXRoIiwiJENvbnRlbnQtVHlwZSIsIiJdLFsiY29udGVudC1sZW5ndGgtcmFuZ2UiLDAsIjIwOTcxNTIwMDAiXSx7InN1Y2Nlc3NfYWN0aW9uX3N0YXR1cyI6IjIwMSJ9LFsic3RhcnRzLXdpdGgiLCIkbmFtZSIsIiJdLFsic3RhcnRzLXdpdGgiLCIkRmlsZW5hbWUiLCIiXV19")
+            "eyJleHBpcmF0aW9uIjoiMjAxNC0wNy0xN1QyMjozMjoxOC4wMDBaIiwiY29uZGl0\
+aW9ucyI6W3siYnVja2V0IjoibGxidWNzIn0seyJhY2wiOiJwcml2YXRlIn0seyJFeHBpcmVzIjoiV\
+Gh1LCAwMSBKYW4gMjAzNyAxNjowMDowMCBHTVQifSxbInN0YXJ0cy13aXRoIiwiJGtleSIsIjIwMT\
+RcL0p1bFwvMTZcL0xpdmVMZWFrLWRvdC1jb20tMmYzXzE0MDU1NjQzMzgiXSxbInN0YXJ0cy13aXR\
+oIiwiJENvbnRlbnQtVHlwZSIsIiJdLFsiY29udGVudC1sZW5ndGgtcmFuZ2UiLDAsIjIwOTcxNTIw\
+MDAiXSx7InN1Y2Nlc3NfYWN0aW9uX3N0YXR1cyI6IjIwMSJ9LFsic3RhcnRzLXdpdGgiLCIkbmFtZ\
+SIsIiJdLFsic3RhcnRzLXdpdGgiLCIkRmlsZW5hbWUiLCIiXV19")
         self.assertEqual(p["signature"], "VufnGKzbNncIeL0AMZ7nWi55FTo=")
